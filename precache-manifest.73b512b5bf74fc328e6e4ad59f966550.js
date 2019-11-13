582aac7c81b90870282ea391ad9443ffb49622b4
Albert Luganga <ahosea15@gmail.com>
2019-11-13 15:45:40 +0300

    Updates

---
+++ precache-manifest.73b512b5bf74fc328e6e4ad59f966550.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d874c47cae96da21a42754afbfe1e5e7",
+    "url": "/unoapp/index.html"
+  },
+  {
+    "revision": "3d19750a4e0cdd35a857",
+    "url": "/unoapp/static/css/main.5989f614.chunk.css"
+  },
+  {
+    "revision": "11b7522e3591e7c12729",
+    "url": "/unoapp/static/js/2.c4a9153d.chunk.js"
+  },
+  {
+    "revision": "3d19750a4e0cdd35a857",
+    "url": "/unoapp/static/js/main.4993bb63.chunk.js"
+  },
+  {
+    "revision": "ce509e9286057546f3d6",
+    "url": "/unoapp/static/js/runtime-main.9f0d9c02.js"
+  }
+]);
